afeb77133ac803e2a50e537e18aec1383cb6544e
halimbezek <halimbezek@gmail.com>
2023-03-27 17:51:43 +0000

    User configuration 1

--- HotelListingApi/Program.cs
+++ HotelListingApi/Program.cs
@@ -2,6 +2,7 @@ using HotelListingApi.Configurations;
 using HotelListingApi.Data;
 using HotelListingApi.Repository;
 using HotelListingApi.RepositoryContracts;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -15,6 +16,11 @@ builder.Services.AddDbContext<HotelListingDbContext>(options =>
 {
     options.UseSqlServer(connectionString);
 });
+
+builder.Services.AddIdentityCore<ApiUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<HotelListingDbContext>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
